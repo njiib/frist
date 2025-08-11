8eb88776258a8c9affb85287645979db7e1c0d81
Najeeb <n.teesan10@gmail.com>
2025-08-12 01:46:50 +0300

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,75 @@
+﻿Config({
+    "network-name": " حديقه عالم الاحلام  ",
+    "service-number": "-",
+    "popup-adv-time": 5,
+    "popup-adv-img-type": true,
+    "popup-adv-repeat-after": 120,
+    "login-type": "user",
+    "news-line": "...:::: مرحباً بكم في حديقة عالم الأحلام الترفيهية! استعدوا لاكتشاف عالم من المتعة والتشويق حيث تتلاقى المغامرات والخيال في مكان واحد. نعدكم بتجربة ترفيهية استثنائية تناسب جميع الأعمار — قريباً نفتح أبوابنا لكم ترقبوا الافتتاح الرسمي... الحلم أصبح قريباً من الواقع !::::...",
+    "input-type": "text",
+    "input-autocomplete": "on",
+    "input-rm-white-spaces": 1,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 1,
+    "input-only-numbers": 0,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 0,
+    "input-to-tel-type-when": 0,
+    "enable-hot-cookie": 1,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 1,
+    "try-count": 5,
+    "warn-when": 3,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+    "price-button": true,
+    "sell-point-button": true,
+    "app-store-status-button": false,
+    "show-date-field": true,
+    "loan-button": false,
+    "redirect-to-esterahah": "",// orWithProtocol: ex, https://google.com leave it blank if dont need esterahah button
+    "redirect-to-mobasher": "",// orWithProtocol: ex, https://google.com leave it blank if dont need mobasher button
+    "app-store-base-url": "",// orWithProtocol: ex, http://192.168.10.10/app/ // leave it blank if locally
+    "profiles": [
+        {
+            "price": " 100 ريال ",
+            "time": "بدون تحديد",
+            "transfer": "350 ميجا ",
+            "validity": "يومين",
+        },
+        
+        {
+            "price": " 200 ريال ",
+            "time": "بدون تحديد",
+            "transfer": "700 ميجا ",
+            "validity": "4 ايام ",
+        },
+        {
+            "price": " 500 ريال ",
+            "time": "بدون تحديد",
+            "transfer": "2000 ميجا",
+            "validity": "اسبوع",
+        },
+        
+
+
+
+
+    ],
+    "sell-points": [
+        { "name": "بقاله الحديقة  " },  
+       
+        { "name": " الكفتيريا  " },
+         { "name": "كشك غزل البنات والفشار  " },
+
+        
+
+
+
+    ]
+})
+
+
+
